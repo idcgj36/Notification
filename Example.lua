666b151be5250e4c7ddd331c0ff789fd5b7cef8d
idcgj36 <anhh20921@gmail.com>
2025-10-09 09:25:01 +0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,10 +1,11 @@
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/idcgj36/Notification/refs/heads/main/Sourced.lua"))()
 
 Notification:Show({
-    Title = "Information",
-    Text = "This is a simple notification",
-    Duration = 5, -- display time in seconds
-    Icon = "rbxassetid://750061419",
-    Background = Color3.fromRGB(0, 0, 0),
-    TextColor = Color3.fromRGB(255, 255, 255)
+	Title = "Information",
+	Text = "This is a simple notification",
+	Duration = 5, -- display time in seconds
+	Icon = "rbxassetid://11558444554",
+	-- IconColor = Color3.fromRGB(255, 0, 0), 
+	Background = Color3.fromRGB(0, 0, 0),
+	TextColor = Color3.fromRGB(0, 255, 255)
 })
